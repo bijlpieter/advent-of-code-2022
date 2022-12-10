0f383fda1881088957aba8fa724d8cc9ee665967
Pieter Bijl <pieterbijl2@gmail.com>
2022-12-10 19:35:15 +0100

    New updates

--- day10.py
+++ day10.py
@@ -4,7 +4,7 @@ class CPU:
 	def __init__(self, fname):
 		self.register = 1
 		self.cycle = 0
-		self.foo = np.ones(240, dtype=int)
+		self.history = np.ones(240, dtype=int)
 
 		with open(fname) as fp:
 			instructions = fp.read().strip().split('\n')
@@ -13,22 +13,22 @@ class CPU:
 			self.execute(instr)
 
 	def execute(self, instr):
-		self.foo[self.cycle] = self.register
+		self.history[self.cycle] = self.register
 		self.cycle += 1
 		
 		op = instr.split(' ')
 		if op[0] == "addx":
-			self.foo[self.cycle] = self.register
+			self.history[self.cycle] = self.register
 			
 			arg = int(op[1])
 			self.register += arg
 			self.cycle += 1
 
-	def history(self):
-		return self.foo.reshape(6, 40)
+	def get_history(self):
+		return self.history.reshape(6, 40)
 
 cpu = CPU("day10.txt")
-history = cpu.history()
+history = cpu.get_history()
 weights = np.arange(20, 240, 40)
 
 print(f"Part 1: {history[:, 19] @ weights}")
