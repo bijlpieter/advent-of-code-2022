d118e5fdbacf95fc26cc3620a932e0fdbc8c2c8d
Pieter Bijl <pieterbijl2@gmail.com>
2022-12-13 17:54:59 +0100

    New updates

--- day13.py
+++ day13.py
@@ -1,56 +1,37 @@
 import numpy as np
+from functools import cmp_to_key
 
-def as_list(val):
-	return val if isinstance(val, list) else [val]
+def aslist(val): return val if isinstance(val, list) else [val]
+def isint(val): return isinstance(val, int)
+def islist(val): return isinstance(val, list)
 
 def compare(left, right):
-	if isinstance(left, int) and isinstance(right, int):
+	if isint(left) and isint(right):
 		return np.sign(left - right)
 
-	if isinstance(left, list) and isinstance(right, list):
+	if islist(left) and islist(right):
 		if not left or not right:
 			return bool(left) - bool(right)
 
 		out = compare(left[0], right[0])
-
 		return out if out else compare(left[1:], right[1:])
 
-	return compare(as_list(left), as_list(right))
-
-class Packet:
-	def __init__(self, pstr):
-		self.x = eval(pstr)
-
-	def __lt__(self, other):
-		return compare(self.x, other.x) == -1
-
-	def __eq__(self, other):
-		return compare(self.x, other.x) == 0
-
-	def __gt__(self, other):
-		return compare(self.x, other.x) == 1
-
-	def __str__(self):
-		return str(self.x)
+	return compare(aslist(left), aslist(right))
 
 with open("day13.txt") as fp:
 	pairs = fp.read().strip().split("\n\n")
 
 lists = [
-	(Packet(left), Packet(right))
+	(eval(left), eval(right))
 	for pair in pairs
 	for left, right in [pair.split("\n")]
 ]
 
-order = np.array([compare(left.x, right.x) for left, right in lists])
-
+order = np.array([compare(left, right) for left, right in lists])
 print(f"Part 1: {np.arange(1, len(order)+1)[order < 0].sum()}")
 
-div_one, div_two = Packet("[[2]]"), Packet("[[6]]")
-
+div_one, div_two = [[2]], [[6]]
 all_lists = [l for pair in lists for l in pair] + [div_one, div_two]
-ordered = sorted(all_lists)
-
+ordered = sorted(all_lists, key=cmp_to_key(compare))
 one, two = ordered.index(div_one) + 1, ordered.index(div_two) + 1
-
 print(f"Part 2: {one * two}")
